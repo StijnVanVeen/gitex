115c6a0858b6a3ab1329fc843bf923a62630fe4e
Stijn van Veen <stijnboz@live.nl>
2018-04-25 14:15:47 +0200

    test

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/hello', (req, res) => {
     res.json({'msg' : 'hello you all'})
 });
 
+app.get('/bye', (req, res) => {
+    res.json({'msg' : 'bye!'})
+});
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () =>{
